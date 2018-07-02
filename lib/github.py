e69982949b64e04e8614c857e8d7a43293e0f427
Johann Bauer <bauerj@bauerj.eu>
2018-07-02 13:13:03 +0200

    Change check if issue is closed

--- lib/github.py
+++ lib/github.py
@@ -19,7 +19,8 @@ def update_issue(id, body):
 
 
 def issue_is_closed(id):
-    return repo.get_issue(id).closed_by is not NotSet
+    closed_by = repo.get_issue(id).closed_by
+    return closed_by is not NotSet and closed_by is not None and hasattr(closed_by, "login")
 
 
 def issue_closed_by(id):
