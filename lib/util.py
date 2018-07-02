8bda3c8e30f62389c9832b1e1f5e1660947f4481
Johann Bauer <bauerj@bauerj.eu>
2018-07-02 13:24:39 +0200

    Allow update to continue on failure

--- lib/util.py
+++ lib/util.py
@@ -1,4 +1,5 @@
 import datetime
+import traceback
 
 from lib import issues, github
 from lib.database import CrashKind
@@ -16,15 +17,17 @@ def get_greeting():
 def update_posts(dry_run):
     kinds = CrashKind.select()
     for k in kinds:
-        if not k.github_id:
-            continue
-        _, body = issues.format_issue(k.id)
-        print("update_issue", k.github_id, body)
-        if not dry_run:
-            github.update_issue(k.github_id, body)
-        if github.issue_is_closed(k.github_id):
-            body = issues.format_reopen_comment(k.id, github.issue_closed_by(k.github_id))
-            if body:
-                print("Respond", k.github_id, body)
-                if not dry_run:
-                    github.respond(k.github_id, body)
+        try:
+            if not k.github_id:
+                continue
+            _, body = issues.format_issue(k.id)
+            if not dry_run:
+                github.update_issue(k.github_id, body)
+            if github.issue_is_closed(k.github_id):
+                body = issues.format_reopen_comment(k.id, github.issue_closed_by(k.github_id))
+                if body:
+                    print("Respond", k.github_id, body)
+                    if not dry_run:
+                        github.respond(k.github_id, body)
+        except:
+            traceback.print_exc()
